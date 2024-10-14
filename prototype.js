f6ba7d84efd728f399d2e51d44516bbf82383fad
Jayawd456 <156908915+Jayawd456@users.noreply.github.com>
2024-10-14 19:17:54 +0530

    Add files via upload

---
+++ prototype.js
@@ -0,0 +1,9 @@
+const namasteBtn = document.getElementById('popup');
+namasteBtn.addEventListener('click', showMsg);
+
+function showMsg(){
+alert("Implementing government regulations and laws that protect forests and enforce penalties for illegal logging Convincing farmers to avoid deforestation and adopt sustainable agriculture and forestry practicesEducating ourselves and others about the importance and value of forests.");
+}
+
+
+ 
